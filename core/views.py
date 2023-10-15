e169f9a6ff5ac61292a6512168005dfd343c8df3
IagoGonçalves22 <iago.g.goncalves@gmail.com>
2023-10-15 09:26:36 -0300

    tabela de produtos estilizado

--- core/views.py
+++ core/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.contrib import messages
 
 from .forms import ContatoForm, ProdutoModelForm
+from .models import Produto
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'produtos': Produto.objects.all()
+    }
+    return render(request, 'index.html', context)
 
 
 def contato(request):
@@ -29,12 +33,8 @@ def produto(request):
     if str(request.method) == 'POST':
         form = ProdutoModelForm(request.POST, request.FILES)
         if form.is_valid():
-            prod = form.save(commit=False)
 
-            print(f'Nome: {prod.nome}')
-            print(f'Preço: {prod.preco}')
-            print(f'Estoque: {prod.estoque}')
-            print(f'Imagem: {prod.imagem}')
+            form.save()
 
             messages.success(request, 'Produto salvo com sucesso!')
             form = ProdutoModelForm()
